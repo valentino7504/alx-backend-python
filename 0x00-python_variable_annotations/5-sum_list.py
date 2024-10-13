a68bb9e559062b18fd45d4b916c251c6ed491883
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-13 23:14:08 +0100

    feat: task 5 solution

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+
+sum list of floats
+
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+    sums a list of floats
+    '''
+    return sum(input_list)
