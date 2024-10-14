5e99bbbe4264f4c47e29c4a42085a2986f23c5b1
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-14 15:59:23 +0100

    feat: task 3 solution

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+
+module to get an asyncio task
+
+'''
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    return asyncio.Task(wait_random(max_delay))
