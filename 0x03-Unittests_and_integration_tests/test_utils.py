942f174c5e8c19809f2442d50ab6a43abf387029
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-04 09:25:06 +0100

    feat: test nested map exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,3 +20,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, n_map: Mapping, p: Iterable, exp: Any):
         '''test access nested map method'''
         self.assertEqual(access_nested_map(n_map, p), exp)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        '''test exceptions for nested map'''
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
