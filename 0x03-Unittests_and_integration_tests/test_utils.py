6a6b4c93bef2f2242da438746a604681a9097516
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-04 08:32:10 +0100

    fix: add type annotations

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,7 @@ test utils
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from typing import Mapping, Iterable, Any
 
 
 class TestAcessNestedMap(unittest.TestCase):
@@ -16,10 +17,6 @@ class TestAcessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, map, path, exp):
+    def test_access_nested_map(self, n_map: Mapping, p: Iterable, exp: Any):
         '''test access nested map method'''
-        self.assertEqual(access_nested_map(map, path), exp)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertEqual(access_nested_map(n_map, p), exp)
