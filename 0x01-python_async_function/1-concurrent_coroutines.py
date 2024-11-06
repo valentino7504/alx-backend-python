1bdf0989a8cf67ed4e98d4ded948d0b6843be16d
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-06 07:43:19 +0100

    refactor: PEP 8 validation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-'''
+"""
 
 execute multiple coroutines simultaneously
 
-'''
+"""
 import asyncio
 from typing import List
 
-
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''
+    """
     wait for multiple concurrent routines
-    '''
+    """
     tasks = []
     completed = []
     for _ in range(n):
